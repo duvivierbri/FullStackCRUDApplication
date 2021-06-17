3ba32689f4f50bd2e2cc104c203bfbfb68eea567
Mariana Vazquez <mariana.vazquez7033@gmail.com>
2021-06-17 12:14:26 -0400

    added student pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+
+import StudentListing from './StudentListing';
+import NewStudentForm from './NewStudentForm';
+import EditStudentForm from './EditStudentForm';
+import SingleViewStudent from './SingleViewStudent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <Route path ="/" exact render = {
+            () => {
+              return (<Home />);
+            }
+          }/>
+          <Route path ="/studentlisting" exact render = {
+            () => {
+              return (<StudentListing />);
+            }
+          }/>
+          <Route path ="/newstudent" exact render = {
+            () => {
+              return (<NewStudentForm />);
+            }
+          }/>
+          <Route path ="/editstudent" exact render = {
+            () => {
+              return (<EditStudentForm />);
+            }
+          }/>
+          <Route path ="/singlestudent" exact render = {
+            () => {
+              return (<SingleViewStudent />);
+            }
+          }/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/newStudentForm.js
@@ -0,0 +1,156 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class NewStudentForm extends Component{
+  constructor(props){
+      super(props);
+      
+      this.state = {
+        firstName: '',
+        lastName: '',
+        gpa: '',
+        email: '',
+        url: '',
+        collegeID: ''
+      }
+
+      this.handleFirstNameInput = this.handleFirstNameInput.bind(this);
+      this.handleLastNameInput = this.handleLastNameInput.bind(this);
+      this.handleGPAInput = this.handleGPAInput.bind(this);
+      this.handleURLInput = this.handleURLInput.bind(this);
+      this.handleEmailInput = this.handleEmailInput.bind(this);
+      this.handleCollegeIDInput = this.handleCollegeIDInput.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleFirstNameInput = (e) => {
+    this.setState({firstName: e.target.value});
+  }
+
+  handleLastNameInput = (e) => {
+    this.setState({lastName: e.target.value});
+  }
+
+  handleGPAInput = (e) => {
+    this.setState({gpa: e.target.value});
+  }
+
+  handleURLInput = (e) => {
+    this.setState({url: e.target.value});
+  }
+
+  handleEmailInput = (e) => {
+    this.setState({email: e.target.value})
+  }
+
+  handleCollegeIDInput = (e) => {
+    this.setState({collegeID: e.target.value})
+  }
+
+  handleSubmit = (event) => {
+    axios.post('http://localhost:3000/addStudents',{
+      'firstName': this.state.firstName,
+      'lastName': this.state.lastName,
+      'url': this.state.url,
+      'gpa': this.state.gpa,
+      'email': this.state.email,
+      'collegeID': this.state.collegeID
+    }).then(response => {
+      console.log(response);
+    }).catch(err => {
+      console.log(err);
+    });
+
+    window.location.replace('studentlisting');
+  }
+
+  render(){
+
+  return(
+    <div className="App">
+      <div className="App-header">
+        <div style={{flexDirection: 'row', margin: '1%'}}>
+          <Link to='/'>
+            <button className="button">
+              Home
+            </button>
+          </Link>
+          {/* <Link to='/campuslisting'>
+            <button className="button">
+              Campuses
+            </button>
+          </Link> */}
+          <Link to='/studentlisting'>
+            <button className="button">
+              Students
+            </button>
+          </Link>
+        </div>
+        <form className="studnentForm">
+          <div className="stackedForm">
+          <h1 style={{color: 'tan'}}> Add New Student </h1>
+            <div className="field">
+                <input
+                  type="text"
+                  name="firstname"
+                  placeholder="Student First Name"
+                  onChange={this.handleFirstNameInput}
+                />
+            </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="lastname"
+                  placeholder="Student Last Name"
+                  onChange={this.handleLastNameInput}
+                />
+            </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Student Email"
+                  onChange={this.handleEmailInput}
+                />
+            </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="gpa"
+                  placeholder="Student GPA"
+                  onChange={this.handleGPAInput}
+                />
+            </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="url"
+                  placeholder="Student URL"
+                  onChange={this.handleURLInput}
+                />
+            </div>
+            <div className="field">
+                <input
+                  type="text"
+                  name="collegeID"
+                  placeholder="Campus ID (if possible)"
+                  onChange={this.handleCollegeIDInput}
+                />
+            </div>
+            <p style={{color: 'black', fontSize: '13px'}}> Campus ID could be left empty, if you are adding from Campus Page. </p>
+
+              <div className="black submit button" onClick={this.handleSubmit}>
+                {/* <i className="plus icon" /> */}
+                Add Student
+              </div>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+}
+
+export default NewStudentForm;
