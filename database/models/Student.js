471954051bcf210f794c0302d9e89d579c6a0d9d
ChrisAndrade <chriskevinandrade@gmail.com>
2021-06-19 18:07:58 -0400

    Added content to Student and index in models folder

---
+++ database/models/Student.js
@@ -0,0 +1,61 @@
+const Sequelize = require('sequelize');
+const db = require('../dbinit');
+
+const Student = db.sequelize.define('student', {
+
+    firstname: {
+
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+
+            notEmpty: true,
+
+        }
+
+    },
+
+    firstname: {
+
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+
+            notEmpty: true,
+
+        }
+
+    },
+
+    email: {
+
+        type: Sequelize.STRING,
+        validate: {
+
+            isEmail: true,
+            notEmpty: true,
+
+        }
+
+    },
+
+    imgUrl: {
+
+        type: Sequelize.STRING,
+        defaultValue: "https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_960_720.png"
+    },
+
+    gpa: {
+
+        type: Sequelize.FLOAT,
+        validate: {
+
+            len: [0.0, 4.0]
+
+        }
+
+    }
+    
+})
+
+module.exports = Student;
