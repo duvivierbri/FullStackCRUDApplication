2db27a279cc89c0a690c02d34efffcf1133c2126
Mariana Vazquez <mariana.vazquez7033@gmail.com>
2021-06-18 21:43:56 -0400

    added new student2.jsand studentADD.js, but incompete

---
+++ src/Student1.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Student1 extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            gpa: '',
+        }
+    }
+
+    
+
+
+}
---
+++ src/Student2js.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Student1 = (props) => (
+    <div className='Student2'>
+        firstName:{props.firstName}
+        lastName: {props.lastName}
+        email: {props.email}
+        gpa: {props.gpa}
+    </div>
+)
+
+export default Student2;
